065f9c7b39b9a99e9062e76ec6cb0f9c43dd912c
Terrance Bryant <tj904615@gmail.com>
2019-09-08 17:41:08 -0500

    Added firebase config to js

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDkPHAG1JyOjawl6tjYli4zpDaAsRQm4PI",
+    authDomain: "trainactivity-210fe.firebaseapp.com",
+    databaseURL: "https://trainactivity-210fe.firebaseio.com",
+    projectId: "trainactivity-210fe",
+    storageBucket: "",
+    messagingSenderId: "355726279667",
+    appId: "1:355726279667:web:ce95dded90cdb566220886"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
+
+  
