510c3a77ab3d90d754039a219e6afeb00618af2e
Terrance Bryant <tj904615@gmail.com>
2019-09-09 12:33:12 -0500

    Added variables

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,4 +11,23 @@ var firebaseConfig = {
   firebase.initializeApp(firebaseConfig);
   var database = firebase.database();
 
-  
+  // Adding trains Button
+  $("#submit").on("click", function() {
+      //Get inputs
+      var name = $("#search-name").val().trim();
+      var destination = $("#search-destination").val().trim();
+      var firstTrain = moment($("#search-time").val().trim(), "HH:mm").format("X");
+      var frequency = $("#search-frequency").val().trim();
+
+      console.log(name);
+      console.log(destination);
+      console.log(firstTrain);
+      console.log(frequency);
+
+      var newTrain = {
+          name: name,
+          destination: destination,
+          firstTrain: firstTrain,
+          frequency: frequency,
+      };
+  })
